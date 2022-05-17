ae4905c5ac299bcadf8db01ef909bbb882736663
NhuHuy <nhudinhhuy09@gmail.com>
2022-05-17 17:57:28 +0700

    Huy: update userCustomSearch

--- src/main/java/com/klpod221/repository/EventRepository.java
+++ src/main/java/com/klpod221/repository/EventRepository.java
@@ -24,7 +24,7 @@ public interface EventRepository extends CrudRepository<EventModel, Long> {
 //customSearchWithStatus
     @Query(value="SELECT * FROM events u WHERE ((u.name LIKE %:search%) OR (u.description LIKE %:search%) OR (u.location LIKE %:search%) OR (u.famous_person LIKE %:search%) OR (u.free_food LIKE %:search%)) AND (u.is_active = :status) ORDER BY id DESC", nativeQuery = true)
     List<EventModel> customSearchWithStatus(@Param("search") String search, @Param("status") int searchStatus);
-
+//userCustomSearch
     @Query(value="SELECT * FROM events u WHERE ((u.name LIKE %:search%) OR (u.description LIKE %:search%) OR (u.location LIKE %:search%) OR (u.famous_person LIKE %:search%) OR (u.free_food LIKE %:search%)) AND (u.is_active = 1) ORDER BY id DESC", nativeQuery = true)
     List<EventModel> userCustomSearch(@Param("search") String search);
 }
